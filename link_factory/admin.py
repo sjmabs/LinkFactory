9e668c22457ee35cb70236e566faf9beba90654c
Shaun <shaun.mabin@hotmail.co.uk>
2023-10-22 15:59:17 +0100

    first commit

---
+++ link_factory/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Link, Profile
+
+# Register your models here.
+admin.site.register(Link)
+admin.site.register(Profile)
---
+++ link_factory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LinkFactoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'link_factory'
